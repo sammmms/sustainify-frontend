a93cd17e55d1a8685613e5881049e8f55f35f6d1
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-06 01:09:41 +0700

    fix: responsive navigation bar, and it's icon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import { RouterView } from "vue-router";
   import NavbarComponent from "./components/NavbarComponent.vue";
+  import FooterComponent from "./components/FooterComponent.vue";
 </script>
 
 <template>
-  <div class="lg:max-w-[1280px] md:max-w-[720px] max-w-[480px] mx-auto px-10">
+  <div class="flex flex-col gap-8 lg:max-w-[1280px] mx-auto px-10">
     <NavbarComponent />
     <RouterView />
+    <FooterComponent />
   </div>
 </template>
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,20 +1,39 @@
 <script setup>
   import { RouterLink } from "vue-router";
-  import LogoSVG from "../assets/long_icon.svg?component";
+  import LongLogoSVG from "../assets/long_icon.svg?component";
+  import DashboardSVG from "../assets/dashboard_icon.svg?component";
+  import ExploreSVG from "../assets/explore_icon.svg?component";
+  import HomepageSVG from "../assets/homepage_icon.svg?component";
+
+  import LogoSVG from "../assets/logo_icon.svg?component";
+  import ProfileSVG from "../assets/profile_icon.svg?component";
 </script>
 
 <template>
-  <nav class="flex justify-between items-center">
+  <nav class="flex h-[100px] justify-between items-center">
     <div class="flex gap-8 items-center">
       <RouterLink to="/">
-        <LogoSVG />
+        <LogoSVG class="lg:hidden" />
+        <LongLogoSVG class="hidden lg:block" />
       </RouterLink>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/dashboard">Dashboard</RouterLink>
-      <RouterLink to="/explore">Explore</RouterLink>
+      <RouterLink to="/">
+        <p class="hidden lg:block text-dark-tea-dark">Homepage</p>
+        <HomepageSVG class="block lg:hidden" />
+      </RouterLink>
+      <RouterLink to="/dashboard">
+        <p class="hidden lg:block text-dark-tea-dark">Dashboard</p>
+        <DashboardSVG class="lg:hidden" />
+      </RouterLink>
+      <RouterLink to="/explore">
+        <p class="hidden lg:block text-dark-tea-dark">Explore</p>
+        <ExploreSVG class="lg:hidden" />
+      </RouterLink>
     </div>
     <div>
-      <RouterLink to="/profile">Profile</RouterLink>
+      <RouterLink to="/profile">
+        <p class="hidden lg:block text-dark-tea-dark">Profile</p>
+        <ProfileSVG class="lg:hidden" />
+      </RouterLink>
     </div>
   </nav>
 </template>
