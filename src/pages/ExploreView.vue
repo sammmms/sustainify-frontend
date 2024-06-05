ad4410e440ec90b5cc58a4fda01aae7212bd72e9
naomehmi <naomiprisella@gmail.com>
2024-06-06 01:50:27 +0700

    change default font to inter

--- src/pages/ExploreView.vue
+++ src/pages/ExploreView.vue
@@ -1,25 +1,18 @@
-<script>
-  import Watermark from "../assets/watermark.svg?component";
+<script setup>
+  import WatermarkSVG from "../assets/watermark.svg?component";
 </script>
 
 <template>
   <!-- navbar -->
   <div>
-    <h1 class="text-4xl font-semibold mt-2 mb-4">Explore</h1>
-    <!-- <iframe src="https://www.google.com/maps/d/embed?mid=1ykhGXbqdErBVO8UXSL9puxTvj--YSQM&ehbc=2E312F&noprof=1" height="480"
+    <h1 class="text-4xl font-semibold mb-4">Explore</h1>
+      <iframe src="https://www.google.com/maps/d/embed?mid=1ykhGXbqdErBVO8UXSL9puxTvj--YSQM&ehbc=2E312F&noprof=1" height="400"
         frameBorder="0"
         style="border: 0;"
         allowfullscreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
-        class="rounded-3xl w-full z-0 relative""></iframe> -->
-      <iframe src="https://www.google.com/maps/d/embed?mid=1ykhGXbqdErBVO8UXSL9puxTvj--YSQM&ehbc=2E312F&noprof=1" height="480"
-        frameBorder="0"
-        style="border: 0;"
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-        class="rounded-3xl w-full z-0"></iframe>
-    <Watermark />
-  </div>
+        class="rounded-3xl w-full z-50"></iframe>
+      </div>
+    <WatermarkSVG class="absolute z-[-1] top-0 w-full opacity-60" />
 </template>
