a7d0a4c1100581f7dd8b0f6a15b3e8e0d67d7304
sammmms <samuelonasis23@gmail.com>
2024-06-05 11:26:27 +0700

    feat: tailwind-css for easier css configuration

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div class="flex">
+      <p>Hello</p>
+      <p>Again</p>
+    </div>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
