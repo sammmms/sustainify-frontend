04c082c81442dc45c6c4e3e73a467e7e68fed060
naomehmi <naomiprisella@gmail.com>
2024-06-07 00:29:38 +0700

    footer

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
 </script>
 
 <template>
-  <div class="flex flex-col gap-8 lg:max-w-[1280px] mx-auto px-10 overflow-hidden relative">
+  <div class="flex flex-col gap-8 lg:max-w-[1280px] mx-auto py-10 px-10 overflow-hidden relative">
     <NavbarComponent />
     <RouterView />
-    <FooterComponent />
     <WatermarkSVG class="absolute z-[-1] top-10 w-full opacity-60 h-auto" />
-  </div>
+    </div>
+  <FooterComponent />
 </template>
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,34 @@
+<script setup>
+  import LongLogoSVG from "../assets/long_icon.svg?component";
+</script>
+
 <template>
-  <div class="flex pt-4 justify-center items-center border-t-2 border-text-2">
-    <p class="">Copyrighted to Sustainify</p>
+  <div class="flex w-full pt-6 justify-center items-center border-t-2 border-text-2 bg-white pb-24">
+     <div class="flex justify-between lg:w-[1280px] px-10">
+      <div class="flex gap-10">
+        <ul class="text-[15px] leading-7">
+          <li><h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">Company</h1></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Explore</a></li>
+        </ul>
+        <ul class="text-[15px] leading-7">
+          <li><h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">Explore</h1></li>
+          <li><a href="#">Redeem</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">People</a></li>
+        </ul>
+        <ul  class="text-[15px] leading-7">
+          <li><h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">Contact Us</h1></li>
+          <li><a href="#">Email</a></li>
+          <li><a href="#">Phone</a></li>
+        </ul>
+      </div>
+      <div>
+        <LongLogoSVG />
+        <p></p>
+      </div>
+     </div>
   </div>
 </template>
