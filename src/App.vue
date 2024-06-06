ee60970515a06f4baae55ce8628afc93811ee16f
naomehmi <naomiprisella@gmail.com>
2024-06-06 22:54:52 +0700

    explore carousel

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   import { RouterView } from "vue-router";
   import NavbarComponent from "./components/NavbarComponent.vue";
   import FooterComponent from "./components/FooterComponent.vue";
+  import WatermarkSVG from "./assets/watermark.svg?component";
 </script>
 
 <template>
-  <div class="flex flex-col gap-8 lg:max-w-[1280px] mx-auto px-10">
+  <div class="flex flex-col gap-8 lg:max-w-[1280px] mx-auto px-10 overflow-hidden relative">
     <NavbarComponent />
     <RouterView />
     <FooterComponent />
+    <WatermarkSVG class="absolute z-[-1] top-10 w-full opacity-60 h-auto" />
   </div>
 </template>
--- src/pages/ExploreView.vue
+++ src/pages/ExploreView.vue
@@ -1,18 +1,26 @@
 <script setup>
-  import WatermarkSVG from "../assets/watermark.svg?component";
+let cards = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let scrollbar = document.getele;
 </script>
 
 <template>
   <!-- navbar -->
   <div>
-    <h1 class="text-4xl font-semibold mb-4">Explore</h1>
-      <iframe src="https://www.google.com/maps/d/embed?mid=1ykhGXbqdErBVO8UXSL9puxTvj--YSQM&ehbc=2E312F&noprof=1" height="400"
-        frameBorder="0"
-        style="border: 0;"
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-        class="rounded-3xl w-full z-50 pointer-events-none"></iframe>
-      </div>
-    <WatermarkSVG class="absolute z-[-1] top-0 w-full opacity-60" />
+    <h1 class="text-4xl font-semibold mb-4 ease-out duration-300">Explore</h1>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1ykhGXbqdErBVO8UXSL9puxTvj--YSQM&ehbc=2E312F&noprof=1"
+      height="400" frameBorder="0" style="border: 0;" allowfullscreen="" loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade" class="rounded-3xl w-full z-50 pointer-events-none"></iframe>
+  </div>
+  <div>
+    <div class="flex w-full overflow-x-auto">
+        <div class="w-[200px] h-[200px] bg-text-dark m-5 flex-none" v-for="card in cards" :key="card">
+          {{ card }}
+        </div>
+    </div>
+    <div class="flex justify-between">
+      <button>prev</button>
+      <button>next</button>
+    </div>
+  </div>
 </template>
