34d8d606b27f1410935dd8524ab04e72170a0f06
naomehmi <naomiprisella@gmail.com>
2024-06-06 23:33:23 +0700

    redeem carousel ih

--- src/pages/ExploreView.vue
+++ src/pages/ExploreView.vue
@@ -1,7 +1,7 @@
 <script setup>
 let cards = [1, 2, 3, 4, 5, 6, 7, 8];
 
-let scrollbar = document.getele;
+let carousel = document.getElementsByClassName("navigate");
 </script>
 
 <template>
@@ -13,14 +13,13 @@ let scrollbar = document.getele;
       referrerpolicy="no-referrer-when-downgrade" class="rounded-3xl w-full z-50 pointer-events-none"></iframe>
   </div>
   <div>
-    <div class="flex w-full overflow-x-auto">
-        <div class="w-[200px] h-[200px] bg-text-dark m-5 flex-none" v-for="card in cards" :key="card">
-          {{ card }}
-        </div>
+    <div class="flex justify-between items-end">
+      <h2 class="ml-2">Redeem</h2>
+      <button class="bg-sage-dark px-[5px] rounded-[5px] h-7 text-[13px]">See More</button>
     </div>
-    <div class="flex justify-between">
-      <button>prev</button>
-      <button>next</button>
+    <div class="flex w-full overflow-x-auto">
+        <div class="container rounded-[10px] w-1/2 md:w-1/5 lg:w-1/5 aspect-square bg-text-dark m-2 flex-none" v-for="card in cards" :key="card"></div>
     </div>
   </div>
+  <p>{{cardWidth}}</p>
 </template>
