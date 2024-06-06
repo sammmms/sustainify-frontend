9eabfbc09a84e66b011ed922773473109b122ed5
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-07 01:01:46 +0700

    fix: clipped background

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,14 @@
 </script>
 
 <template>
-  <div class="flex flex-col gap-8 lg:max-w-[1280px] mx-auto py-10 px-10 overflow-hidden relative">
-    <NavbarComponent />
-    <RouterView />
-    <WatermarkSVG class="absolute z-[-1] top-10 w-full opacity-60 h-auto" />
+  <div class="relative">
+    <div
+      class="flex flex-col gap-8 lg:max-w-[1280px] mx-auto px-10 overflow-hidden"
+    >
+      <NavbarComponent />
+      <RouterView />
+      <FooterComponent />
     </div>
-  <FooterComponent />
+    <WatermarkSVG class="absolute z-[-1] top-10 w-full opacity-60 h-auto" />
+  </div>
 </template>
