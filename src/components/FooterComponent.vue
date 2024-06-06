d241ade1f40cd92b636649b7aebbac7e9ee5764e
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-07 01:02:10 +0700

    fix: footer router

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,32 +3,46 @@
 </script>
 
 <template>
-  <div class="flex w-full pt-12 justify-center items-center border-t-2 border-text-2 bg-white pb-32">
-     <div class="flex justify-between lg:w-[1280px] px-10">
+  <div
+    class="flex w-full pt-12 justify-center items-center border-t-2 border-text-2 bg-white pb-32"
+  >
+    <div class="flex justify-between lg:w-[1280px] px-10">
       <div class="flex gap-10">
         <ul class="text-[15px] leading-7">
-          <li><h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">Company</h1></li>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Dashboard</a></li>
-          <li><a href="#">Explore</a></li>
+          <li>
+            <h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">
+              Company
+            </h1>
+          </li>
+          <li><RouterLink to="/">Homepage</RouterLink></li>
+          <li><RouterLink to="/dashboard">Dashboard</RouterLink></li>
+          <li><RouterLink to="/explore">Explore</RouterLink></li>
+          <li><RouterLink to="/about-us">About Us</RouterLink></li>
         </ul>
         <ul class="text-[15px] leading-7">
-          <li><h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">Explore</h1></li>
-          <li><a href="#">Redeem</a></li>
-          <li><a href="#">Events</a></li>
-          <li><a href="#">People</a></li>
+          <li>
+            <h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">
+              Explore
+            </h1>
+          </li>
+          <li><RouterLink to="/explore/redeem">Redeem</RouterLink></li>
+          <li><RouterLink to="/explore/events">Events</RouterLink></li>
+          <li><RouterLink to="/explore/people">People</RouterLink></li>
         </ul>
-        <ul  class="text-[15px] leading-7">
-          <li><h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">Contact Us</h1></li>
-          <li><a href="#">Email</a></li>
-          <li><a href="#">Phone</a></li>
+        <ul class="text-[15px] leading-7">
+          <li>
+            <h1 class="text-[21px] text-dark-tea-dark font-semibold pb-2">
+              Contact Us
+            </h1>
+          </li>
+          <li><RouterLink to="/contact-us/email">Email</RouterLink></li>
+          <li><RouterLink to="/contact-us/phone">Phone</RouterLink></li>
         </ul>
       </div>
       <div>
         <LongLogoSVG />
         <p></p>
       </div>
-     </div>
+    </div>
   </div>
 </template>
