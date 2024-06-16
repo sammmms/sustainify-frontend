a74ffff8e7a9d66e5b6b116357b70d43464d84c5
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-17 02:50:28 +0700

    feat: icon navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,12 +1,17 @@
 <script setup>
-  import { RouterLink } from "vue-router";
+  import { RouterLink, useRouter } from "vue-router";
   import LongLogoSVG from "../assets/long_icon.svg?component";
-  import DashboardSVG from "../assets/dashboard_icon.svg?component";
-  import ExploreSVG from "../assets/explore_icon.svg?component";
-  import HomepageSVG from "../assets/homepage_icon.svg?component";
+  import NavbarWithLogo from "./NavbarWithLogo.vue";
+
+  import HomepageSVGLarge from "../assets/homepage_icon_large.svg?component";
+  import DashboardSVGLarge from "../assets/dashboard_icon_large.svg?component";
+  import ExploreSVGLarge from "../assets/explore_icon_large.svg?component";
+  import ProfileSVGLarge from "../assets/profile_icon_large.svg?component";
 
   import LogoSVG from "../assets/logo_icon.svg?component";
   import ProfileSVG from "../assets/profile_icon.svg?component";
+
+  const router = useRouter();
 </script>
 
 <template>
@@ -17,30 +22,34 @@
         <LongLogoSVG class="hidden lg:block" />
       </RouterLink>
       <RouterLink to="/">
-        <p class="hidden lg:block text-dark-tea-dark size-medium font-semibold">
-          Homepage
-        </p>
-        <HomepageSVG class="block lg:hidden" />
+        <HomepageSVGLarge class="block lg:hidden" />
+        <NavbarWithLogo title="Homepage" iconsPath="homepage_icon" path="/" />
       </RouterLink>
       <RouterLink to="/dashboard">
-        <p class="hidden lg:block text-dark-tea-dark size-medium font-semibold">
-          Dashboard
-        </p>
-        <DashboardSVG class="lg:hidden" />
+        <DashboardSVGLarge class="lg:hidden" />
+        <NavbarWithLogo
+          title="Dashboard"
+          iconsPath="dashboard_icon"
+          path="/dashboard"
+        />
       </RouterLink>
       <RouterLink to="/explore">
-        <p class="hidden lg:block text-dark-tea-dark size-medium font-semibold">
-          Explore
-        </p>
-        <ExploreSVG class="lg:hidden" />
+        <ExploreSVGLarge class="lg:hidden" />
+        <NavbarWithLogo
+          title="Explore"
+          iconsPath="explore_icon"
+          path="/explore"
+        />
       </RouterLink>
     </div>
     <div>
       <RouterLink to="/profile">
-        <p class="hidden lg:block text-dark-tea-dark size-large font-semibold">
-          Profile
-        </p>
-        <ProfileSVG class="lg:hidden" />
+        <ProfileSVGLarge class="lg:hidden" />
+        <NavbarWithLogo
+          title="Profile"
+          iconsPath="profile_icon"
+          path="/profile"
+        />
       </RouterLink>
     </div>
   </nav>
---
+++ src/components/NavbarWithLogo.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { useRouter } from "vue-router";
+  import { defineAsyncComponent } from "vue";
+  const props = defineProps(["title", "iconsPath", "path"]);
+  const NavbarWithLogo = defineAsyncComponent(() =>
+    import(`../assets/${props.iconsPath}.svg?component`)
+  );
+
+  const router = useRouter();
+</script>
+<template>
+  <div
+    v-if="router.currentRoute.value.fullPath == props.path"
+    class="hidden lg:flex gap-2 px-4 py-2 rounded-full bg-dark-tea-dark"
+  >
+    <div class="w-7 h-7 rounded-full bg-white flex justify-center items-center">
+      <NavbarWithLogo />
+    </div>
+    <p class="text-white size-small font-semibold">{{ title }}</p>
+  </div>
+  <div
+    v-if="router.currentRoute.value.fullPath != props.path"
+    class="hidden lg:flex gap-2 px-4 py-2 rounded-full bg-matcha-dark"
+  >
+    <div class="w-7 h-7 rounded-full bg-white flex justify-center items-center">
+      <NavbarWithLogo />
+    </div>
+    <p class="text-white size-small font-semibold">{{ title }}</p>
+  </div>
+</template>
