8e6e250c9ae2d3d83aa9184627380af323d6bc98
Sam! <156488637+sammmms@users.noreply.github.com>
2024-06-17 02:52:00 +0700

    chore: prettify work

--- src/components/ExploreCarousel.vue
+++ src/components/ExploreCarousel.vue
@@ -1,21 +1,51 @@
 <script setup>
   const props = defineProps({
-    title:String,
-    cards:Array,
-    round:String
+    title: String,
+    cards: Array,
+    round: String,
   });
 
-let carousel = document.getElementsByClassName("navigate");
+  let carousel = document.getElementsByClassName("navigate");
 </script>
 
 <template>
   <div>
     <div class="flex justify-between items-end">
       <h2 class="ml-2 font-medium">{{ title }}</h2>
-      <button class="bg-sage-dark px-[5px] rounded-[5px] h-7 text-[13px] mr-2 text-dark-tea">See More</button>
+      <button
+        class="bg-sage-dark px-[5px] rounded-[5px] h-7 text-[13px] mr-2 text-dark-tea"
+      >
+        See More
+      </button>
     </div>
     <div class="flex w-full overflow-x-auto mr-2">
-        <img :class="'container rounded-' + round + ' w-1/2 md:w-1/5 lg:w-1/5 aspect-square bg-white mx-4 my-2 flex-none object-cover'" v-for="card in cards" :key="card" :src="card" />
+      <img
+        :class="
+          'container rounded-' +
+          round +
+          ' w-1/2 md:w-1/5 lg:w-1/5 aspect-square bg-white mx-4 my-2 flex-none object-cover'
+        "
+        v-for="card in cards"
+        :key="card"
+        :src="card"
+      />
     </div>
   </div>
 </template>
+
+<style>
+  ::-webkit-scrollbar {
+    height: 6px;
+    width: 3px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: #d4d4d4;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #94b56d;
+    border-radius: 10px;
+  }
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,51 +1,67 @@
 <script setup>
-let images = [
-  "https://images.pexels.com/photos/3735215/pexels-photo-3735215.jpeg?auto=compress&cs=tinysrgb&w=600",
-  "https://images.pexels.com/photos/20720316/pexels-photo-20720316/free-photo-of-flower-near-moss-on-tree.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/3735218/pexels-photo-3735218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/7512846/pexels-photo-7512846.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-]
-import { ref } from 'vue';
+  let images = [
+    "https://images.pexels.com/photos/3735215/pexels-photo-3735215.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/20720316/pexels-photo-20720316/free-photo-of-flower-near-moss-on-tree.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3735218/pexels-photo-3735218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/7512846/pexels-photo-7512846.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+  import { ref } from "vue";
 
-const count = ref(0);
+  const count = ref(0);
 
-setInterval(() => {
-  if(count.value <= 2){
-    count.value++;
-  } else{
-    count.value = 0;
-  }
-}, 5000);
+  setInterval(() => {
+    if (count.value <= 2) {
+      count.value++;
+    } else {
+      count.value = 0;
+    }
+  }, 5000);
 </script>
 
 <template>
   <div class="flex flex-col gap-12">
     <!-- The first section -->
     <div class="flex flex-col md:flex-row gap-4 h-[400px] md:h-[480px]">
-      <div class="flex-1 bg-white shadow-md rounded-xl flex flex-col justify-evenly">
-        <div class="p-10 font-semibold sm:text-[60px] text-[30px]">Let's Save the World Together</div>
+      <div
+        class="flex-1 bg-white shadow-md rounded-xl flex flex-col justify-evenly"
+      >
+        <div class="p-10 font-semibold sm:text-[60px] text-[30px]">
+          Let's Save the World Together
+        </div>
         <div class="p-10 text-[18px] text-dark-tea-dark underline">
           <RouterLink to="/about-us">More about us &gt;</RouterLink>
         </div>
       </div>
-      <Transition mode="out-in" class="flex-1 bg-text-dark shadow-md rounded-xl w-[40vw]">
-          <img class="w-[40vw] object-cover" :key="count" :src="images[count]" />
+      <Transition
+        mode="out-in"
+        class="flex-1 bg-text-dark shadow-md rounded-xl w-[40vw]"
+      >
+        <img class="w-[40vw] object-cover" :key="count" :src="images[count]" />
       </Transition>
     </div>
 
     <!-- The second section -->
     <div
-      class="flex flex-col md:flex-row w-full h-[400px] md:h-[480px] lg:p-6 bg-white/30 shadow-md rounded-3xl backdrop-blur-lg">
+      class="flex flex-col md:flex-row w-full h-[400px] md:h-[480px] lg:p-6 bg-white/30 shadow-md rounded-3xl backdrop-blur-lg"
+    >
       <div class="flex-1"></div>
-      <div class="bg-matcha-dark rounded-3xl flex-1 order-first max-h-[300px] md:max-h-none md:order-last"></div>
+      <div
+        class="bg-matcha-dark rounded-3xl flex-1 order-first max-h-[300px] md:max-h-none md:order-last"
+      ></div>
     </div>
 
     <!-- The third section -->
     <div class="flex flex-col gap-8">
       <!-- The carousel -->
-      <div class="relative h-[480px] w-full bg-text-dark rounded-3xl hidden md:block">
-        <div class="absolute h-6 w-6 rounded-full mx-4 bg-white right-0 top-1/2" />
-        <div class="absolute h-6 w-6 rounded-full mx-4 bg-white left-0 top-1/2" />
+      <div
+        class="relative h-[480px] w-full bg-text-dark rounded-3xl hidden md:block"
+      >
+        <div
+          class="absolute h-6 w-6 rounded-full mx-4 bg-white right-0 top-1/2"
+        />
+        <div
+          class="absolute h-6 w-6 rounded-full mx-4 bg-white left-0 top-1/2"
+        />
       </div>
 
       <!-- The headings -->
@@ -58,18 +74,27 @@ setInterval(() => {
       </span>
 
       <!-- The cards -->
-      <div class="justify-between h-[200px] gap-4 overflow-x-auto mb-6 hidden md:flex">
+      <div
+        class="justify-between h-[200px] gap-4 overflow-x-auto mb-6 hidden md:flex"
+      >
         <div v-for="i in 5" class="flex flex-col min-w-[200px] w-[240px]">
           <div class="w-full flex-1 bg-matcha-dark shadow-md rounded-xl"></div>
-          <div class="w-full h-[20px] bg-matcha-dark shadow-md rounded-md mt-2"></div>
+          <div
+            class="w-full h-[20px] bg-matcha-dark shadow-md rounded-md mt-2"
+          ></div>
         </div>
       </div>
 
       <!-- The cards explanation -->
-      <div v-for="i in 5" class="flex flex-col md:flex-row gap-4 md:max-h-[220px] overflow-clip">
+      <div
+        v-for="i in 5"
+        class="flex flex-col md:flex-row gap-4 md:max-h-[220px] overflow-clip"
+      >
         <div class="flex flex-col w-full md:w-[210px] h-[300px] md:h-[200px]">
           <div class="flex-1 w-full bg-matcha-dark shadow-md rounded-xl"></div>
-          <div class="w-full h-[20px] bg-matcha-dark shadow-md rounded-md mt-2"></div>
+          <div
+            class="w-full h-[20px] bg-matcha-dark shadow-md rounded-md mt-2"
+          ></div>
         </div>
         <p class="flex-1 text-wrap text-justify overflow-y-auto">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
@@ -89,13 +114,13 @@ setInterval(() => {
 </template>
 
 <style scoped>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 0.5s ease;
+  }
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
 </style>
