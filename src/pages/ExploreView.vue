85d8ad32ef9c904c246722365a4f96c62193b421
naomehmi <naomiprisella@gmail.com>
2024-06-16 22:24:16 +0700

    images

--- src/pages/ExploreView.vue
+++ src/pages/ExploreView.vue
@@ -1,15 +1,36 @@
 <script setup>
- import ExploreCarousel from "../components/ExploreCarousel.vue";
+import ExploreCarousel from "../components/ExploreCarousel.vue";
 
- let cards1 = [1, 2, 3, 4, 5, 6, 7, 8];
- let cards2 = [1, 2, 3, 4, 5, 6, 7, 8];
- let cards3 = [
+let cards1 = [
+  "https://m.media-amazon.com/images/I/31r03j-5WcL.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIuMGU1COKZuYODREqW0BGn4rPgV6wHd3FiA&s",
+  "https://livekortti.fi/pl/starbucks-gift-card-5-usd-us-68789.jpg",
+  "https://play-lh.googleusercontent.com/vAycVoO8oR-1r4ioT-gqNAENjzvCTcnR1Gb9Y1PJaNU2UQnSps-K6K-KvUglc33LROg",
+  "https://cdn.icon-icons.com/icons2/2845/PNG/512/zara_logo_icon_181327.png",
+  "https://i.pinimg.com/564x/49/41/d4/4941d4c5c9c194fa533f50984042f667.jpg",
+  "https://i0.wp.com/glutenfreesleuth.com/wp-content/uploads/2023/04/panda.webp?fit=800%2C416&ssl=1",
+  "https://img.cryptorefills.com/logos_v2/hm.webp",
+];
+let cards2 = [
+  "https://images.pexels.com/photos/9034660/pexels-photo-9034660.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/5487071/pexels-photo-5487071.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/7656742/pexels-photo-7656742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/7656719/pexels-photo-7656719.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/9543414/pexels-photo-9543414.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/8543606/pexels-photo-8543606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  "https://images.pexels.com/photos/12568264/pexels-photo-12568264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/9475434/pexels-photo-9475434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+];
+let cards3 = [
+  "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   "https://images.pexels.com/photos/4927361/pexels-photo-4927361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/3619947/pexels-photo-3619947.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600",
   "https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600",
   "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600",
- ];
+];
 </script>
 
 <template>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,33 +1,37 @@
+<script setup>
+import { VueSlickCarousel } from "vue-slick-carousel";
+</script>
+
 <template>
   <div class="flex flex-col gap-12">
     <!-- The first section -->
     <div class="flex flex-col md:flex-row gap-4 h-[400px] md:h-[480px]">
-      <div class="flex-1 bg-white shadow-md rounded-xl"></div>
-      <div class="flex-1 bg-text-dark shadow-md rounded-xl"></div>
+      <div class="flex-1 bg-white shadow-md rounded-xl">
+        <div class="p-10 font-semibold text-[30px]">Let's Save the World Together</div>
+      </div>
+      <div class="flex-1 bg-text-dark shadow-md rounded-xl">
+        <VueSlickCarousel :arrows="true" :dots="true">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </VueSlickCarousel>
+      </div>
     </div>
 
     <!-- The second section -->
     <div
-      class="flex flex-col md:flex-row w-full h-[400px] md:h-[480px] lg:p-6 bg-white/30 shadow-md rounded-3xl backdrop-blur-lg"
-    >
+      class="flex flex-col md:flex-row w-full h-[400px] md:h-[480px] lg:p-6 bg-white/30 shadow-md rounded-3xl backdrop-blur-lg">
       <div class="flex-1"></div>
-      <div
-        class="bg-matcha-dark rounded-3xl flex-1 order-first max-h-[300px] md:max-h-none md:order-last"
-      ></div>
+      <div class="bg-matcha-dark rounded-3xl flex-1 order-first max-h-[300px] md:max-h-none md:order-last"></div>
     </div>
 
     <!-- The third section -->
     <div class="flex flex-col gap-8">
       <!-- The carousel -->
-      <div
-        class="relative h-[480px] w-full bg-text-dark rounded-3xl hidden md:block"
-      >
-        <div
-          class="absolute h-6 w-6 rounded-full mx-4 bg-white right-0 top-1/2"
-        />
-        <div
-          class="absolute h-6 w-6 rounded-full mx-4 bg-white left-0 top-1/2"
-        />
+      <div class="relative h-[480px] w-full bg-text-dark rounded-3xl hidden md:block">
+        <div class="absolute h-6 w-6 rounded-full mx-4 bg-white right-0 top-1/2" />
+        <div class="absolute h-6 w-6 rounded-full mx-4 bg-white left-0 top-1/2" />
       </div>
 
       <!-- The headings -->
@@ -40,27 +44,18 @@
       </span>
 
       <!-- The cards -->
-      <div
-        class="justify-between h-[200px] gap-4 overflow-x-auto mb-6 hidden md:flex"
-      >
+      <div class="justify-between h-[200px] gap-4 overflow-x-auto mb-6 hidden md:flex">
         <div v-for="i in 5" class="flex flex-col min-w-[200px] w-[240px]">
           <div class="w-full flex-1 bg-matcha-dark shadow-md rounded-xl"></div>
-          <div
-            class="w-full h-[20px] bg-matcha-dark shadow-md rounded-md mt-2"
-          ></div>
+          <div class="w-full h-[20px] bg-matcha-dark shadow-md rounded-md mt-2"></div>
         </div>
       </div>
 
       <!-- The cards explanation -->
-      <div
-        v-for="i in 5"
-        class="flex flex-col md:flex-row gap-4 md:max-h-[220px] overflow-clip"
-      >
+      <div v-for="i in 5" class="flex flex-col md:flex-row gap-4 md:max-h-[220px] overflow-clip">
         <div class="flex flex-col w-full md:w-[210px] h-[300px] md:h-[200px]">
           <div class="flex-1 w-full bg-matcha-dark shadow-md rounded-xl"></div>
-          <div
-            class="w-full h-[20px] bg-matcha-dark shadow-md rounded-md mt-2"
-          ></div>
+          <div class="w-full h-[20px] bg-matcha-dark shadow-md rounded-md mt-2"></div>
         </div>
         <p class="flex-1 text-wrap text-justify overflow-y-auto">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
